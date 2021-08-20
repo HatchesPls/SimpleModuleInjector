13dc2ba64c86cca83c2aadbc82541447a4d8f98a
HatchesPls <hatchespls@protonmail.com>
2021-08-20 03:11:15 +0200

    Build number to 1.1.0.0

--- Header/Main.h
+++ Header/Main.h
@@ -15,10 +15,9 @@
 #include "ThirdParty/ImGui/imgui_impl_dx11.h"
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "Shlwapi.lib")
-#pragma comment(lib, "Comdlg32.lib")
 #include "resource.h"
 
-static const std::string SMI_BUILD = "1.0.0.3";
+static const std::string SMI_BUILD = "1.1.0.0";
 
 namespace Injector
 {
